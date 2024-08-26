a34c3b1f2cdd451f86f9737a9d7e7366cd2043df
Santi <santiagoamallo@gmail.com>
2024-08-26 18:48:28 -0300

    Coordinate systems and Sizing

--- entry_santigame.c
+++ entry_santigame.c
@@ -1,47 +1,76 @@
 
 int entry(int argc, char **argv) {
-	
-	// This is how we (optionally) configure the window.
-	// You can set this at any point in the runtime and it will
-	// be applied in os_update().
-	// If you don't care, you can ignore all of this as it all
-	// has reasonable default values.
-	window.title = STR("Minimal Game Example");
-	window.scaled_width = 1280; // We need to set the scaled size if we want to handle system scaling (DPI)
-	window.scaled_height = 720; 
+
+	window.title = fixed_string("El Santi Juego");
+	window.width = 1280; // We need to set the scaled size if we want to handle system scaling (DPI)
+	window.height = 720; 
 	window.x = 200;
 	window.y = 90;
-	window.clear_color = hex_to_rgba(0x6495EDff);
-	window.allow_resize = true;
-	window.fullscreen = false;
-	
+	window.clear_color = hex_to_rgba(0x272736ff);
+
+	Gfx_Image* player = load_image_from_disk(fixed_string("player.png"), get_heap_allocator());
+	assert(player, "fuckyty wackyty stuff");
+
+	Vector2 player_pos = v2(0, 0);
+	float64 seconds_counter = 0.0;
+	s32 frame_count = 0;
+
 	float64 last_time = os_get_elapsed_seconds();
+
 	while (!window.should_close) {
 		reset_temporary_storage();
 		
+		draw_frame.projection = m4_make_orthographic_projection(window.width * -0.5, window.width * 0.5, window.height * -0.5, window.height * 0.5, -1, 10);
+
+		float zoom = 5.3;
+		draw_frame.camera_xform = m4_make_scale(v3(1.0/zoom, 1.0/zoom, 1.0));
+		//draw_frame.view = m4_make_scale(v3(1.0/5.3, 1.0/5.3, 1.0));
+		//hacer art con aseprite y lo que hizo el rando
+
 		float64 now = os_get_elapsed_seconds();
-		if ((int)now != (int)last_time) log("%.2f FPS\n%.2fms", 1.0/(now-last_time), (now-last_time)*1000);
+		float64 delta_t = now - last_time;
 		last_time = now;
-		
-		Matrix4 rect_xform = m4_scalar(1.0);
-		rect_xform         = m4_rotate_z(rect_xform, (f32)now);
-		rect_xform         = m4_translate(rect_xform, v3(-.25f, -.25f, 0));
-		draw_rect_xform(rect_xform, v2(.5f, .5f), COLOR_GREEN);
-		
-		draw_rect(v2(sin(now), -.8), v2(.5, .25), COLOR_RED);
-		
-		float aspect = (f32)window.width/(f32)window.height;
-		float mx = (input_frame.mouse_x/(f32)window.width  * 2.0 - 1.0)*aspect;
-		float my = input_frame.mouse_y/(f32)window.height * 2.0 - 1.0;
-		
-		draw_line(v2(-.75, -.75), v2(mx, my), 0.005, COLOR_WHITE);
-		
-		if (is_key_just_pressed('F')) {
-			window.fullscreen = !window.fullscreen;
+
+		os_update(); 
+
+		if (is_key_just_pressed(KEY_ESCAPE)) {
+			window.should_close = true;
 		}
 		
-		os_update(); 
+
+		Vector2 input_axis = v2(0, 0);
+		if (is_key_down('A')) {
+			input_axis.x -= 1.0;
+		}
+		if (is_key_down('D')) {
+			input_axis.x += 1.0;
+		}
+		if (is_key_down('S')) {
+			input_axis.y -= 1.0;
+		}
+		if (is_key_down('W')) {
+			input_axis.y += 1.0;
+		}
+
+		input_axis = v2_normalize(input_axis);
+		
+		player_pos = v2_add(player_pos, v2_mulf(input_axis, 30.0 * delta_t));
+
+		Vector2 size = v2(6.0, 8.0);
+		Matrix4 xform = m4_scalar(1.0);
+		xform         = m4_translate(xform, v3(player_pos.x, player_pos.y, 0));
+		xform         = m4_translate(xform, v3(size.x * -0.5, 0.0, 0));
+
+		draw_image_xform(player, xform, size, COLOR_WHITE);
+
 		gfx_update();
+		seconds_counter += delta_t;
+		frame_count += 1;
+		if (seconds_counter > 1.0) {
+			log("fps: %i", frame_count);
+			seconds_counter = 0.0;
+			frame_count = 0;
+		}
 	}
 
 	return 0;
