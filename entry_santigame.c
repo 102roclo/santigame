becd3915a59f11fd1cfaae6e578bdfc2ba20e249
Santi <santiagoamallo@gmail.com>
2024-08-27 13:04:53 -0300

    entity structure & sprite system

--- entry_santigame.c
+++ entry_santigame.c
@@ -1,3 +1,85 @@
+#define MAX_ENTITY_COUNT 1024
+
+typedef struct Sprite {
+	Gfx_Image* image;
+	Vector2 size;
+}	Sprite;
+
+typedef enum SpriteID {
+	SPRITE_nil,
+	SPRITE_player,
+	SPRITE_tree0,
+	SPRITE_tree1,
+	SPRITE_flower0,
+	SPRITE_rock,
+	SPRITE_MAX,
+}SpriteID;
+Sprite sprites[SPRITE_MAX];
+
+Sprite* get_sprite(SpriteID id) {
+	if (id >= 0 && id < SPRITE_MAX) {
+		return &sprites[id];
+	}
+	return &sprites[0];
+}
+
+typedef enum EntityArchetype {
+	arch_nil = 0,
+	arch_player = 1,
+	arch_rock = 2,
+	arch_tree = 3,
+	arch_flower = 4,
+} EntityArchetype;
+
+
+typedef struct Entity {
+bool is_valid;
+EntityArchetype arch;
+Vector2 pos;
+
+bool render_sprite;
+SpriteID sprite_id;
+} Entity;
+
+typedef struct World {
+	Entity entities[MAX_ENTITY_COUNT];
+} World;
+World* world = 0;
+
+Entity* entity_create() {
+	Entity* entity_found = 0;
+	for (int i= 0; i < MAX_ENTITY_COUNT; i++) {
+		Entity* existing_entity = &world->entities[i];
+		if (!existing_entity->is_valid){
+			entity_found = existing_entity;
+			break;
+		}	
+	}
+	assert(entity_found != NULL && "No more free entities!");
+	entity_found->is_valid = true;
+	return entity_found;
+}
+
+void entity_destroy(Entity* entity) {
+	memset(entity, 0, sizeof(Entity));
+}
+
+void setup_flower(Entity* en) {
+	en->arch = arch_flower;
+	en->sprite_id = SPRITE_flower0;
+	//en->sprite_id = SPRITE_flower1;
+}
+
+void setup_tree(Entity* en) {
+	en->arch = arch_tree;
+	en->sprite_id = SPRITE_tree0;
+	//en->sprite_id = SPRITE_tree1;
+}
+
+void setup_player(Entity* en) {
+	en->arch = arch_player;
+	en->sprite_id = SPRITE_player;
+}
 
 int entry(int argc, char **argv) {
 
@@ -8,10 +90,34 @@ int entry(int argc, char **argv) {
 	window.y = 90;
 	window.clear_color = hex_to_rgba(0x272736ff);
 
-	Gfx_Image* player = load_image_from_disk(fixed_string("player.png"), get_heap_allocator());
-	assert(player, "fuckyty wackyty stuff");
+	world = alloc(get_heap_allocator(), sizeof(World));
+	memset(world, 0, sizeof(World));
 
-	Vector2 player_pos = v2(0, 0);
+	sprites[SPRITE_player] = (Sprite){ .image=load_image_from_disk(STR("player.png"), get_heap_allocator()), .size=v2(6.0, 8.0) };
+	sprites[SPRITE_tree0] = (Sprite){ .image=load_image_from_disk(STR("tree0.png"), get_heap_allocator()), .size=v2(9.0, 16.0) };
+	 if (sprites[SPRITE_tree0].image == NULL) {
+        log("Error loading tree0.png");
+    }
+	sprites[SPRITE_tree1] = (Sprite){ .image=load_image_from_disk(STR("tree1.png"), get_heap_allocator()), .size=v2(9.0, 14.0) };
+	sprites[SPRITE_flower0] = (Sprite){ .image=load_image_from_disk(STR("flower0.png"), get_heap_allocator()), .size=v2(5.0, 7.0) };
+	if (sprites[SPRITE_flower0].image == NULL) {
+        log("Error loading flower0.png");
+    }
+
+	Entity* player_en = entity_create();
+	setup_player(player_en);
+	
+	for (int i = 0; i < 10; i++){
+		Entity* en = entity_create();
+		setup_flower(en);
+		en->pos = v2(get_random_float32_in_range(-1, 1), get_random_float32_in_range(-1, 1));
+	}
+	for (int i = 0; i < 10; i++){
+		Entity* en = entity_create();
+		setup_tree(en);
+		en->pos = v2(get_random_float32_in_range(-1, 1), get_random_float32_in_range(-1, 1));
+	}
+	
 	float64 seconds_counter = 0.0;
 	s32 frame_count = 0;
 
@@ -24,14 +130,31 @@ int entry(int argc, char **argv) {
 
 		float zoom = 5.3;
 		draw_frame.camera_xform = m4_make_scale(v3(1.0/zoom, 1.0/zoom, 1.0));
-		//draw_frame.view = m4_make_scale(v3(1.0/5.3, 1.0/5.3, 1.0));
-		//hacer art con aseprite y lo que hizo el rando
 
 		float64 now = os_get_elapsed_seconds();
 		float64 delta_t = now - last_time;
 		last_time = now;
 
 		os_update(); 
+		//render
+		for (int i = 0; i < MAX_ENTITY_COUNT; i++) {
+			Entity* en = &world->entities[i];
+			if(en->is_valid) {
+				switch (en->arch) {
+					
+					default:
+					{
+						Sprite* sprite = get_sprite(en->sprite_id);
+						Matrix4 xform = m4_scalar(1.0);
+						xform         = m4_translate(xform, v3(en->pos.x, en->pos.y, 0));
+						xform         = m4_translate(xform, v3(sprite->size.x * -0.5, 0.0, 0));
+						draw_image_xform(sprite->image, xform, sprite->size, COLOR_WHITE);
+					break;					
+					}
+					
+				}
+			}
+		}
 
 		if (is_key_just_pressed(KEY_ESCAPE)) {
 			window.should_close = true;
@@ -54,14 +177,12 @@ int entry(int argc, char **argv) {
 
 		input_axis = v2_normalize(input_axis);
 		
-		player_pos = v2_add(player_pos, v2_mulf(input_axis, 30.0 * delta_t));
+		player_en->pos = v2_add(player_en->pos, v2_mulf(input_axis, 30.0 * delta_t));
 
 		Vector2 size = v2(6.0, 8.0);
 		Matrix4 xform = m4_scalar(1.0);
-		xform         = m4_translate(xform, v3(player_pos.x, player_pos.y, 0));
+		xform         = m4_translate(xform, v3(player_en->pos.x, player_en->pos.y, 0));
 		xform         = m4_translate(xform, v3(size.x * -0.5, 0.0, 0));
-
-		draw_image_xform(player, xform, size, COLOR_WHITE);
 
 		gfx_update();
 		seconds_counter += delta_t;
